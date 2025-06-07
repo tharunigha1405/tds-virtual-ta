a8e7d638ddab27786df669cc8f570425eefe010b
tharunigha1405 <23f3003634@ds.study.iitm.ac.in>
2025-06-07 19:08:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = "https://tds.s-anand.net/#/2025-01/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+with open("data/course/tds_content.html", "w", encoding='utf-8') as f:
+    f.write(soup.prettify())
